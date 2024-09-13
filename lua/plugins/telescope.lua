f2bd49431236b1c55b9214ca1ae2e581f029fb5c
srijang <srijang@uber.com>
2024-09-13 11:20:35 +0000

    [fix] fixed grep search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
--- local builtin = require("telescope.builtin")
--- vim.keymap.set("n", "<leader>fg", function()
---   builtin.grep_string({ search = vim.fn.input("Grep > ") })
--- end, { desc = "Search by Grep" })
--- return { {} }
---
--- return { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>f/", function()
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end, { desc = "Search by Grep" })
+
+return { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
